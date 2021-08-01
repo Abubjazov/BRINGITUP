99928ad46b3a84aaf28398c4f43ca29804a1a16f
Radik Abubjazov <abubjazov@mail.ru>
2021-08-02 01:32:24 +0300

    добавлена обработка ошибок во все модули проекта

--- src/js/modules/difference.js
+++ src/js/modules/difference.js
@@ -2,8 +2,12 @@ export default class Difference {
     constructor(oldOfficer, newOfficer, officerItems){
         this.oldOfficer = document.querySelector(oldOfficer);
         this.newOfficer = document.querySelector(newOfficer);
-        this.oldItems = this.oldOfficer.querySelectorAll(officerItems);
-        this.newItems = this.newOfficer.querySelectorAll(officerItems);
+
+        try {
+            this.oldItems = this.oldOfficer.querySelectorAll(officerItems);
+            this.newItems = this.newOfficer.querySelectorAll(officerItems);
+        } catch(e) {}
+
         this.officerItems = officerItems;
         this.oldCounter = 0;
         this.newCounter = 0;
@@ -12,25 +16,29 @@ export default class Difference {
 
 
     bindTrigger(officer, counter, items) {
-        officer.querySelector('.plus').addEventListener('click', () => {
-            if ( counter !== items.length - 2) {
-                items[counter].classList.add('animated', 'fadeIn');
-                items[counter].style.display = 'flex';
-                counter++;
-            } else {
-                items[counter].classList.add('animated', 'fadeIn');
-                items[counter].style.display = 'flex';              
-                items[counter + 1].remove();
-            }
-        });
+        try {
+            officer.querySelector('.plus').addEventListener('click', () => {
+                if ( counter !== items.length - 2) {
+                    items[counter].classList.add('animated', 'fadeIn');
+                    items[counter].style.display = 'flex';
+                    counter++;
+                } else {
+                    items[counter].classList.add('animated', 'fadeIn');
+                    items[counter].style.display = 'flex';              
+                    items[counter + 1].remove();
+                }
+            });
+        } catch(e) {}
     }
 
     hideItems(items) {
-        items.forEach((item, i, arr) => {
-            if (i !== arr.length - 1) {
-                item.style.display = 'none';
-            }
-        });
+        try {
+            items.forEach((item, i, arr) => {
+                if (i !== arr.length - 1) {
+                    item.style.display = 'none';
+                }
+            });
+        } catch(e) {}
     }
 
     init() {
